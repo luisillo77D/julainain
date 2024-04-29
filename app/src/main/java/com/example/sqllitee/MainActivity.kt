5f9fafc8c4057395074263b2f55ad8e9924b2188
luisillo77D <luisillo77mario@gmail.com>
2024-04-28 17:51:24 -0700

    add the fucking toolbar que el pendejo del julian no explica en el pdf del año del caldo, ojala no nos quiera revisar el pendejo

--- app/src/main/java/com/example/sqllitee/MainActivity.kt
+++ app/src/main/java/com/example/sqllitee/MainActivity.kt
@@ -1,22 +1,101 @@
 package com.example.sqllitee
 
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ListView
+import android.widget.SearchView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity(var adapter: NotasAdapter?=null) : AppCompatActivity() {
 
     var listaDeNotas = ArrayList<Notas>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar) // Define el Toolbar como la barra de acción
+
+        listaDeNotas.add(Notas(1,"primer titulo","primer descripcion"))
+
+        adapter = NotasAdapter(this,listaDeNotas)
+        var listView : ListView = findViewById(R.id.listView)
+        listView.adapter = adapter
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu,menu)
+
+        val buscar = menu!!.findItem(R.id.app_bar_search).actionView as SearchView
+        val manejador = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        buscar.setSearchableInfo(manejador.getSearchableInfo(componentName))
+        buscar.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                Toast.makeText(applicationContext,query,Toast.LENGTH_LONG).show()
+                return  false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item!!.itemId){
+            R.id.menuAgregar ->{
+                var intent = Intent(this, AddActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+    class NotasAdapter(contexto: Context, var listaDeNotas:ArrayList<Notas>):BaseAdapter(){
+        var contexto: Context?=contexto
+        override fun getCount(): Int {
+            return listaDeNotas.size
+        }
+
+        override fun getItem(position: Int): Any {
+            return listaDeNotas[position]
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            val nota =listaDeNotas[position]
+            val inflater = contexto!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            val miVista=inflater.inflate(R.layout.molde_notas,null)
+
+            miVista.findViewById<TextView>(R.id.textViewTitulo).setText(nota.titulo)
+            miVista.findViewById<TextView>(R.id.textViewContenido).setText(nota.descripcion)
+
+            return miVista
+        }
+
     }
 }
